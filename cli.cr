d7e9dc548a78a3b962ec74b17f41ff80a77b0ff8
_ <53088156+636f7374@users.noreply.github.com>
2020-04-26 22:39:20 +0800

    Support exclude needles feature & Command line Fixed.

--- cli.cr
+++ cli.cr
@@ -1,18 +1,18 @@
-require "durian"
+require "./src/coffee.cr"
 
 case ARGV[0_i32]?
 when "version", "--version", "-v"
   puts <<-EOF
 Version:
-  Coffee.cr - Cloudflare Edge Server Scanner
-  _Version_ - #{Coffee::VERSION} (2020.04.11)
+  Coffee.cr - Cloudflare Edge Server Booster
+  _Version_ - #{Coffee::VERSION} (2020.04.26)
 EOF
 when "help", "--help", "-h"
   puts <<-EOF
 Usage: coffee [command] [--] [arguments]
 Command:
   version, --version, -v  Display Version Information of Coffee.cr
-  help, --help, -h        Show this Coffee: Cloudflare Edge Server Scanner Help
+  help, --help, -h        Show this Coffee: Cloudflare Edge Server Booster Help
 Options:
   --disable-progress-bar  Disable drawing Progress Bar
   --import                Import scanner Configuration File
--- src/coffee/serialization/option.cr
+++ src/coffee/serialization/option.cr
@@ -5,6 +5,7 @@ module Coffee::Serialization
     property ipRange : String
     property export : String?
     property needles : String | Array(String)
+    property excludes : Exclude?
     property timeout : TimeOut?
     property type : String
 
@@ -12,63 +13,63 @@ module Coffee::Serialization
       @ipRange = String.new
       @export = String.new
       @needles = String.new
+      @excludes = Exclude.new
       @timeout = TimeOut.new
       @type = String.new
     end
 
-    def iata_needles? : Array(Needle::IATA)?
+    def to_iata_needles?(needles : String | Array(String), flag : Needle::Flag) : Array(Needle::IATA)?
       list = [] of Needle::IATA
-      _needles = needles
 
-      case Needle::Flag.parse? type
-      when Needle::Flag::IATA
-        case _needles
+      case flag
+      when .iata?
+        case needles
         when Array(String)
-          _needles.each do |item|
+          needles.each do |item|
             _item = Needle::IATA.parse? item
 
             list << _item if _item
           end
         when String
-          _item = Needle::IATA.parse? _needles
+          _item = Needle::IATA.parse? needles
 
           list << _item if _item
         else
         end
-      when Needle::Flag::Edge
-        case _needles
+      when .edge?
+        case needles
         when Array(String)
-          _needles.each do |item|
+          needles.each do |item|
             _edge = Needle::Edge.parse? item
             _item = _edge.to_iata? if _edge
 
             list << _item if _item
           end
         when String
-          _edge = Needle::Edge.parse? _needles
+          _edge = Needle::Edge.parse? needles
           _item = _edge.to_iata? if _edge
 
           list << _item if _item
         else
         end
-      when Needle::Flag::Region
-        case _needles
+      when .region?
+        case needles
         when Array(String)
-          _needles.each do |item|
+          needles.each do |item|
             _region = Needle::Region.parse? item
             next unless _region
 
             _region.each { |item| list << item }
           end
         when String
-          _region = Needle::Region.parse? _needles
+          _region = Needle::Region.parse? needles
 
           _region.try &.each { |item| list << item }
         else
         end
-      else Nil
       end
 
+      return if list.empty?
       list
     end
 
@@ -80,8 +81,21 @@ module Coffee::Serialization
       Writer.new _export
     end
 
+    def exclude_needles(iatas : Array(Needle::IATA)) : Array(Needle::IATA)
+      return iatas unless _excludes = excludes
+      return iatas unless flag = Needle::Flag.parse? _excludes.type
+      return iatas unless _excludes = to_iata_needles? _excludes.needles, flag
+
+      _excludes.each { |exclude| iatas.delete exclude }
+
+      iatas
+    end
+
     def to_task(writer : Writer? = nil, command_line : Bool = false) : Task?
-      return unless needles = iata_needles?
+      return unless flag = Needle::Flag.parse? type
+      return unless _needles = to_iata_needles? needles, flag
+
+      _needles = exclude_needles _needles
 
       _ip_range = IPAddress.new ipRange rescue nil
       return unless _ip_range
@@ -89,10 +103,22 @@ module Coffee::Serialization
       writer = export_to_writer unless writer
       _timeout = timeout || TimeOut.new
 
-      task = Task.new _ip_range, needles, command_line, _timeout
+      task = Task.new _ip_range, _needles, command_line, _timeout
       task.writer = writer if writer
 
       task
+    end
+
+    class Exclude
+      include YAML::Serializable
+
+      property needles : String | Array(String)
+      property type : String
+
+      def initialize
+        @needles = String.new
+        @type = String.new
+      end
     end
   end
 end
